175f66233a29ce86cb12c371f74e36e03ca0c438
Psychonic Silver <nickolasknowles1920@gmail.com>
2025-05-29 22:36:18 -0400

    Update search.js

--- search.js
+++ search.js
@@ -30,7 +30,7 @@ animeContainer.parentNode.insertBefore(paginationContainer, animeContainer.nextS
 let animeList = [];
 let selectedGenres = new Set();
 let currentPage = 1;
-const maxPerPage = 56; // API seems capped at 50 max results per page
+const maxPerPage = 56;
 let perPage = 56;
 let totalPages = 1;
 
